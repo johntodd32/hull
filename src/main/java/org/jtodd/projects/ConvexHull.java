c965ee1b60d7c3d0c193d26041f013bc169badd6
John Todd <John.Todd@ilwllc.com>
2017-12-28 12:25:57 -0500

    Create hulls for simple cases

--- src/main/java/org/jtodd/projects/ConvexHull.java
+++ src/main/java/org/jtodd/projects/ConvexHull.java
@@ -1,14 +1,32 @@
 package org.jtodd.projects;
 
+import java.awt.geom.Area;
+import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class ConvexHull {
+
+    public static Path2D calculate(Set<Point2D> points) {
+        Path2D path = new Path2D.Double();
+        if (points.size() > 0) {
+            Iterator<Point2D> pi = points.iterator();
+            Point2D start = pi.next();
+            path.moveTo(start.getX(), start.getY());
+            while (pi.hasNext()) {
+                Point2D p = pi.next();
+                path.lineTo(p.getX(), p.getY());
+            }
+            path.closePath();
+        }
+        return path;
+    }
 
     /**
      * Return the rightmost (greatest X coordinate) of a set of points.
--- src/test/java/org/jtodd/projects/TestConvexHull.java
+++ src/test/java/org/jtodd/projects/TestConvexHull.java
@@ -1,13 +1,73 @@
 package org.jtodd.projects;
 
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Path2D;
+import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestConvexHull {
+
+    private static final AffineTransform IDENTITY_TRANSFORM = new AffineTransform();
+
+    @Test
+    public void testHullOfEmptySet() {
+        Path2D hull = ConvexHull.calculate(new HashSet<>());
+        Assert.assertTrue(comparePaths(new Path2D.Double(), hull));
+    }
+
+    @Test
+    public void testHullOfTwoPoints() {
+        Point2D p1 = new Point2D.Double(0, 0);
+        Point2D p2 = new Point2D.Double(1, 1);
+        Set<Point2D> s = new HashSet<>();
+        s.add(p1);
+        s.add(p2);
+
+        Path2D expectedHull = new Path2D.Double();
+        expectedHull.moveTo(p1.getX(), p1.getY());
+        expectedHull.lineTo(p2.getX(), p2.getY());
+        expectedHull.closePath();
+
+        Path2D foundHull = ConvexHull.calculate(s);
+        boolean passed = comparePaths(expectedHull, foundHull);
+        Assert.assertTrue(passed);
+    }
+
+    public static boolean comparePaths(Path2D expectedHull, Path2D foundHull) {
+        PathIterator pe = expectedHull.getPathIterator(IDENTITY_TRANSFORM);
+        pe.next();
+        PathIterator pf = foundHull.getPathIterator(IDENTITY_TRANSFORM);
+        pf.next();
+        boolean passed = true;
+        double [] expectedCoords = new double[6];
+        double [] foundCoords = new double[6];
+        int expectedType;
+        int foundType;
+        for (; (!pe.isDone() || !pf.isDone()) && passed; pe.next(), pf.next()) {
+            expectedType = pe.currentSegment(expectedCoords);
+            foundType = pf.currentSegment(foundCoords);
+            if ((pe.isDone() && !pf.isDone()) || (!pe.isDone() && pf.isDone())) {
+                System.out.printf("Areas have different lengths");
+                passed = false;
+            } else if (expectedType != foundType) {
+                System.err.println("Areas have sides of different types");
+                passed = false;
+            } else if (expectedCoords[0] != foundCoords[0]) {
+                System.err.printf("Areas have different coordinates: (%f,%f) != (%f,%f)\n", expectedCoords[0], expectedCoords[1], foundCoords[0], foundCoords[1]);
+                passed = false;
+            } else if (expectedCoords[1] != foundCoords[1]) {
+                System.err.printf("Areas have different coordinates: (%f,%f) != (%f,%f)\n", expectedCoords[0], expectedCoords[1], foundCoords[0], foundCoords[1]);
+                passed = false;
+            }
+        }
+        return passed;
+    }
 
     @Test
     public void testFindRightmostPointInASetOfPoints() {
